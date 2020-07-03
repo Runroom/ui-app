a92277ca0425aa36af9f1819487a489cd0aec8d3
Jose <jose@runroom.com>
2020-07-03 11:55:14 +0200

    Iterates Iframe component

--- components/Iframe/index.js
+++ components/Iframe/index.js
@@ -1,32 +1,34 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+// import axios from 'axios';
+import React from 'react';
+// import React, { useEffect, useState } from 'react';
 
 import { proxy } from '../../package.json';
 import Iframe from './styles';
 
 const Code = ({ component }) => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
-  const Loader = () => <div>Loading {component}.twig...</div>;
-  const Error = () => <div>Error {component}.twig!</div>;
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [isError, setIsError] = useState(false);
+  // const Loader = () => <div>Loading {component}.twig...</div>;
+  // const Error = () => <div>Error {component}.twig!</div>;
 
-  useEffect(() => {
-    (async () => {
-      try {
-        await axios.get(`/.netlify/functions/server/components/${component}`);
-      } catch (err) {
-        console.error(err);
-        setIsError(true);
-      } finally {
-        setIsLoading(false);
-      }
-    })();
-  }, []);
+  // useEffect(() => {
+  //   (async () => {
+  //     try {
+  //       await axios.get(`/.netlify/functions/server/components/${component}`);
+  //     } catch (err) {
+  //       console.error(err);
+  //       setIsError(true);
+  //     } finally {
+  //       setIsLoading(false);
+  //     }
+  //   })();
+  // }, []);
 
-  return isLoading ? (
-    <Loader />
-  ) : isError ? <Error /> : (
-      <Iframe src={`${proxy[process.env.NODE_ENV]}/${component}.html`} frameBorder='0'></Iframe>
-    );
+  // return isLoading ? (
+  //   <Loader />
+  // ) : isError ? <Error /> : (
+  //     <Iframe src={`${proxy[process.env.NODE_ENV]}/${component}.html`} frameBorder='0'></Iframe>
+  //   );
+  return <Iframe src={`${proxy[process.env.NODE_ENV]}/${component}.html`} frameBorder='0'></Iframe>;
 };
 export default Code;
